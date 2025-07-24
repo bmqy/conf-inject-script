9f843c0f21c99932ae2a6014dde5b609c892fefb
bmqy <bmqy@qq.com>
2025-07-24 13:46:21 +0800

    update

--- utils/build.js
+++ utils/build.js
@@ -7,7 +7,21 @@
  * 使用方法: node build.js
  */
 
-const fs = require('fs');
+import fs from 'fs';
+import { joinPaths } from './build.js'; // 假设 joinPaths 已定义
+
+// 示例：读取文件
+async function readFileAsync(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) reject(err);
+      else resolve(data);
+    });
+  });
+}
+
+// 导出函数
+export { readFileAsync, joinPaths };
 
 // 手动实现路径拼接
 export function joinPaths(...parts) {
